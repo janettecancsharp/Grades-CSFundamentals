f6c33aba51c926fc12d2a83bd7f271c24f3f61b8
Janette Griggs <janette.can.csharp@gmail.com>
2016-01-11 09:08:56 -0800

    Control Flow

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,18 +23,28 @@ namespace Grades
             }
             set
             {
-                if (!String.IsNullOrEmpty(value))
+                if (String.IsNullOrEmpty(value))
                 {
-                    if (_name != value)
-                    {
-                        NameChangedEventArgs args = new NameChangedEventArgs();
-                        args.ExistingName = _name;
-                        args.NewName = value;
-
-                        NameChanged(this, args);
-                    }
-                    _name = value;
+                    throw new ArgumentException("Name cannot be null or empty");
                 }
+
+                if (_name != value && NameChanged != null)
+                {
+                    NameChangedEventArgs args = new NameChangedEventArgs();
+                    args.ExistingName = _name;
+                    args.NewName = value;
+
+                    NameChanged(this, args); // should check if null
+                }
+                _name = value;
+            }
+        }
+
+        public void WriteGrades(TextWriter destination)
+        {
+            for (int i = 0; i < grades.Count; i++)
+            {
+                destination.WriteLine(grades[i]);
             }
         }
 
@@ -53,7 +64,7 @@ namespace Grades
             GradeStatistics stats = new GradeStatistics();
             float sum = 0;
 
-            foreach(float grade in grades)
+            foreach (float grade in grades)
             {
                 stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
                 stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 //using System.Speech.Synthesis;
 using System.Text;
@@ -18,21 +19,64 @@ namespace Grades
 
             //book.NameChanged += new NameChangedDelegate(OnNameChanged); // verbose
             //book.NameChanged += new NameChangedDelegate(OnNameChanged2); // verbose
-            book.NameChanged += OnNameChanged;
-            book.NameChanged += OnNameChanged2;
+            //book.NameChanged += OnNameChanged;
+            //book.NameChanged += OnNameChanged2;
             //book.NameChanged = null; // this is not valid for events
 
-            book.Name = "Scott's Grade Book";
-            book.Name = "My Grade Book";
-            book.AddGrade(91);
-            book.AddGrade(89.5f);
-            book.AddGrade(75);
+            //book.Name = "Scott's Grade Book";
+            //book.Name = "My Grade Book";
 
+            GetBookName(book);
+            AddGrades(book);
+            SaveGrades(book);
+            WriteResults(book);
+        }
+
+        private static void WriteResults(GradeBook book)
+        {
             GradeStatistics stats = book.ComputeStatistics();
 
             WriteResult("Average", stats.AverageGrade);
             WriteResult("Highest", (int)stats.HighestGrade);
             WriteResult("Lowest", stats.LowestGrade);
+            WriteResult(stats.Description, stats.LetterGrade);
+        }
+
+        private static void SaveGrades(GradeBook book)
+        {
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+                book.WriteGrades(outputFile);
+                //outputFile.Close();
+            }
+        }
+
+        private static void AddGrades(GradeBook book)
+        {
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+        }
+
+        private static void GetBookName(GradeBook book)
+        {
+            try
+            {
+                Console.Write("Enter a name: ");
+                book.Name = Console.ReadLine();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Something was null");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong");
+            }
         }
 
         static void WriteResult(string description, int result)
@@ -47,6 +91,11 @@ namespace Grades
 
             // C# 6.0
             Console.WriteLine($"{description}: {result:F2}");
+        }
+
+        static void WriteResult(string description, string result)
+        {
+            Console.WriteLine($"{description}: {result}");
         }
 
         static void OnNameChanged(object sender, NameChangedEventArgs args)
